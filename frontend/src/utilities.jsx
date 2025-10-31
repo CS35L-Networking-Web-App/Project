a76c605f42d5af733f8e60a6b3f2bfee5a8e07c5
Estrella Castro <estrellacastro@Estrellas-MacBook-Air.local>
2025-10-31 08:39:40 -0700

    fixed Sign up button so more sign up requests can't be sent after it has already been clicked

--- frontend/src/utilities.jsx
+++ frontend/src/utilities.jsx
@@ -1,50 +1,19 @@
 import { useForm } from "react-hook-form";
-import { useState } from "react";
-import { useNavigate } from "react-router-dom";
 
-export default function SignUp(){
-
-    const navigate = useNavigate();
-    const [signUpMessage, setSignUpMessage] = useState("");
-    const [success, setSuccess] = useState(false);
-
+export function HookForm({head, action, finished, textBelow, submission}){
     const{
-    register,
-    handleSubmit, 
-    formState:{errors, isSubmitting},
-    setError, 
-  } = useForm(); 
+        register,
+        handleSubmit, 
+        formState:{errors, isSubmitting},
+        setValue,
+        setError, 
+      } = useForm(); 
 
-  const onSubmit = async({email, password}) => {
-
-    try{
-      console.log("Sign-up Submitted:", {email, password});
-      const response = await fetch("https://jsonplaceholder.typicode.com/posts",{
-      method: "POST",
-      headers: {"Content-Type": "application/json",},
-      body: JSON.stringify({email, password}),
-      });
-
-    const result = await response.json();
-
-    if (!response.ok) throw new Error("Failed to submit data.");
-
-    setSignUpMessage("Account created successfully!");
-    setSuccess(true);
-    setTimeout(()=> {navigate("/");}, 3000);
-
-} catch (error){
-    setError("root", { message: "Data could not be submitted."});
-    setSignUpMessage(error.message);
-    setSuccess(false);
-    }
-};
-    return (
-    <div className='flex items-center justify-center min-h-screen'>
-    <form
+return(
+<form
      className="p-6 space-y-4 bg-white rounded-lg shadow w-full max-w-sm"
-     onSubmit={handleSubmit(onSubmit)}>
-      <h1 className="text-2xl font-semibold mb-4">Join now!</h1>
+     onSubmit={handleSubmit(submission)}>
+      <h1 className="text-2xl font-semibold mb-4">{head}</h1>
       <div>
         <label
         htmlFor="email"
@@ -71,13 +40,12 @@ export default function SignUp(){
         className={`bg-gray-50 border text-gray-900 rounded-lg focus:border-primary-600 block w-full p-2.5 ${errors.password ? "border-red-500 focus:ring-red-500" : "border-gray-300 focus:ring-primary-600"}`}
         />{errors.password && <p className="text-red-500">{errors.password.message}</p>}
       </div>
-      {signUpMessage && (<p className={`text-center ${success ? "text-indigo-700" : "text-red-500"}`}>{signUpMessage}</p>)}
+      {textBelow}
       <button
       type="submit"
       className="w-full text-white bg-blue-600 hover:bg-blue-700 rounded-lg px-5 py-2.5"
-      disabled={isSubmitting || success}
-      >Sign Up</button>
+      disabled={{finished} || isSubmitting}
+      >{action}</button>
      </form>     
-    </div>
-  );
-};
+);
+}
